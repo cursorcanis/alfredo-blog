e9605cd7f52d64df96582f9cffd74a8989b670d0
unknown <alfredoalea@gmail.com>
2020-09-23 16:20:47 -0400

    Second Commit

---
+++ src/build/precache-manifest.512dfffb81b2e15014fdbc7425530feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ef1ffd3d2037fd4b54308631bcf07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49906f8696b4451c8a2b",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "7bdb57c84ceac01269ad",
+    "url": "/static/js/2.0c47b585.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c47b585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49906f8696b4451c8a2b",
+    "url": "/static/js/main.414c1f6c.chunk.js"
+  },
+  {
+    "revision": "6ec610b256ad2fa06c98",
+    "url": "/static/js/runtime-main.e7054a9b.js"
+  }
+]);
